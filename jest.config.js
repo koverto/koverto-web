78681f8abbd92e8b7c46edb23f18893b8115448e
Jesse B. Hannah <jesse@jbhannah.net>
2020-03-04 10:18:31 -0700

    Mock Tailwind imports in Jest

--- jest.config.js
+++ jest.config.js
@@ -83,6 +83,7 @@ module.exports = {
   // A map from regular expressions to module names that allow to stub out resources with a single module
   moduleNameMapper: {
     "\\.css$": "<rootDir>/__mocks__/styleMock.js",
+    "^tailwindcss/": "<rootDir>/__mocks__/styleMock.js",
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
